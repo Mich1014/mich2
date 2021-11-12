812e135a39239c6ed1656d6cec97e28c8f774f98
Mich1014 <93336029+Mich1014@users.noreply.github.com>
2021-11-12 11:59:41 -0400

    agregué el css

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -6,7 +6,110 @@
     <link rel="stylesheet" href="style2.css">
     <title>Wattpad</title>
     <style>
+*{box-sizing:border-box;}
 
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: url("form.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+form{
+    width:500px;
+    
+	padding:80px;
+	border-radius:10px;
+    margin:auto;
+    margin-top: 80px;
+    background-color:rgba(255, 255, 255, 0.753);
+    -webkit-box-shadow: 0px 0px 8px 2px #000000; 
+    box-shadow: 0px 0px 8px 2px #000000;
+}
+
+form label{
+	width:72px;
+	font-weight:bold;
+	display:inline-block;
+}
+
+form input[type="text"],
+form input[type="email"]{
+	width:180px;
+	padding:3px 10px;
+	border:1px solid #f6f6f6;
+	border-radius:3px;
+	background-color:#f6f6f646;
+	margin:8px 0;
+    display:inline-block;
+    border: solid 1px black;
+}
+
+form input[type="submit"]{
+	width:100%;
+	padding:8px 16px;
+	margin-top:32px;
+	border: solid 0px black;
+	border-radius:5px;
+	display:block;
+	color:#fff;
+    background-color:rgb(89, 165, 28);
+    box-shadow: 0px 0px 8px 1px #000000; 
+    -webkit-box-shadow: 0px 0px 8px 1px #000000; 
+    -moz-box-shadow: 0px 0px 8px 1px #000000; 
+    font-weight: bold;
+} 
+
+form input[type="reset"]{
+	width:100%;
+	padding:8px 16px;
+	margin-top:32px;
+	border: solid 0px black;
+	border-radius:5px;
+	display:block;
+	color:#fff;
+    background-color:rgb(165, 28, 28);
+    box-shadow: 0px 0px 8px 1px #000000; 
+    -webkit-box-shadow: 0px 0px 8px 1px #000000; 
+    -moz-box-shadow: 0px 0px 8px 1px #000000; 
+    font-weight: bold;
+} 
+
+
+
+form input[type="submit"]:hover{
+	cursor:pointer;
+}
+
+textarea{
+	width:100%;
+	height:100px;
+
+	border-radius:3px;
+	background-color:#f6f6f646;			
+	margin:8px 0;
+	/*resize: vertical | horizontal | none | both*/
+	resize:none;
+    display:block;
+    
+}
+form .nacimiento {
+    border: solid 1px black;
+    border-radius:3px;
+    padding:3px 10px;
+    background-color:#f6f6f646;
+}
+
+.parteabajo {
+    display: flex;
+    justify-content: space-around;
+
+}
+
+
+.parteabajo p{
+    padding: 0 40px 20px 40px;
+}
         </style>
     </head>
 <body>
